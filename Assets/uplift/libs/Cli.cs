e8adcceec82380dc4e511badea247094b73f39d2
Przemysław Kamiński <przemyslaw@kaminski.se>
2017-06-05 15:16:11 +0200

    Add Cli handling

---
+++ Assets/uplift/libs/Cli.cs
@@ -0,0 +1,32 @@
+using Schemas;
+using UnityEngine;
+
+namespace Uplift
+{
+
+    public static class Cli
+    {
+        public static string LastArgument() {
+            string[] args = System.Environment.GetCommandLineArgs();
+            return args[args.Length - 1];
+        }
+        public static void InstallDependencies() {
+            UpfileHandler handler = UpfileHandler.Instance();
+            handler.InstallDependencies();
+        }
+
+        public static void UpdatePackage() {
+            string packageName = LastArgument();
+
+            PackageRepo pr = PackageList.Instance().GetLatestPackage(packageName);
+            LocalHandler.UpdatePackage(pr);
+
+        }
+
+        public static void NukePackage() {
+            string packageName = LastArgument();
+            LocalHandler.NukePackage(packageName);
+        }
+    }
+
+}

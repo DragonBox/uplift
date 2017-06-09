5567df0684b9ed5705e8bd51b7decbb68ea83776
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-06-09 10:11:13 +0200

    Take note of an idea

--- Assets/Plugins/Editor/Uplift/Common/IRepositoryHandler.cs
+++ Assets/Plugins/Editor/Uplift/Common/IRepositoryHandler.cs
@@ -4,6 +4,8 @@ namespace Uplift.Common
     
     internal interface IRepositoryHandler {
         Upset[] ListPackages();
+        // IDEA: we could also use, and let the caller manager / dispose the target
+        // void DownloadPackageTo(Upset package, TemporaryDirectory target);
         TemporaryDirectory DownloadPackage(Upset package);
     }
 }

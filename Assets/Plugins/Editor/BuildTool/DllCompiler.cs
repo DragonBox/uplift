4f832c533df1273db121484b24a836e24b3fd966
Jerome Lacoste <jerome.lacoste@gmail.com>
2017-10-26 08:27:26 +0200

    DllCompiler in C#

---
+++ Assets/Plugins/Editor/BuildTool/DllCompiler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Uplift.Common;
+using System.Linq;
+
+namespace BuildTool {
+	public class DllCompiler {
+		[MenuItem("Uplift/DllCompiler/Test", false, 0)]
+		public static void Test() {
+			UnityInstallation unity = UnityInstallationUtils.Current ();
+			BuildLibraryData Data = new BuildLibraryData ();
+			Data.References = new string[] {
+				unity.Paths().Managed("UnityEditor.dll"),
+				unity.Paths().Managed("UnityEngine.dll"),
+				Helper.PathCombine("Assets", "Plugins", "Editor", "SharpCompress.dll")
+			};
+			List<string> FileList = new List<string> ();
+			FileList.AddRange (FileSystemUtil.GetFiles (Helper.PathCombine ("Assets", "Plugins", "Editor", "Uplift")).Where (f => f.EndsWith(".cs") && !f.Contains ("Testing")));
+			FileList.AddRange (FileSystemUtil.GetFiles (Helper.PathCombine ("Assets", "Plugins", "Editor", "BuildTool")).Where (f => f.EndsWith(".cs")));
+			FileList.AddRange (FileSystemUtil.GetFiles (Helper.PathCombine ("Assets", "Plugins", "Editor", "UnityHacks")).Where (f => f.EndsWith(".cs")));
+			Data.Files = FileList.ToArray ();
+			Data.SdkLevel = 2;
+			Data.OutFile = "target/Uplift.dll";
+
+			unity.BuildLibrary(Data);
+		}
+	}
+}
---
+++ Assets/Plugins/Editor/BuildTool/Helper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+namespace BuildTool
+{
+	public class Helper
+	{
+		public static bool isMac() {
+			return Application.platform == RuntimePlatform.OSXEditor;
+		}
+
+		public static string PathCombine(params string[] values) {
+			return string.Join (Path.DirectorySeparatorChar.ToString (), values);
+		}
+
+		public static string ArgEscape(string s) {
+			// FIXME for spaces
+			if (s.Contains (" ")) {
+				Debug.LogWarning ("ArgEscape not yet implemented yet file " + s + " contains spaces");
+			}
+			return s;
+		}
+	
+		public static void RunCommand(string ExeName, string[] args) {
+			using (var process = new System.Diagnostics.Process ()) {
+				process.StartInfo.FileName = ExeName;
+				process.StartInfo.Arguments = string.Join (" ", args);
+				process.StartInfo.RedirectStandardError = true;
+				process.StartInfo.RedirectStandardOutput = true;
+				process.StartInfo.UseShellExecute = false;
+
+				StringBuilder stdout = new StringBuilder ();
+				StringBuilder stderr = new StringBuilder ();
+
+				process.OutputDataReceived += (sender, e) => {
+					if (e.Data == null)
+						return;
+					stdout.Append (e.Data).Append ("\n");
+				};
+				process.ErrorDataReceived += (sender, e) => {
+					if (e.Data == null)
+						return;
+					stderr.Append (e.Data).Append ("\n");
+				};
+				process.EnableRaisingEvents = true;
+
+				Debug.Log ("Running " + process.StartInfo.FileName + " " + process.StartInfo.Arguments);
+
+				process.Start ();
+
+				process.BeginOutputReadLine ();
+				process.BeginErrorReadLine ();
+
+				process.WaitForExit ();
+
+				string output = stdout.ToString ();
+				if (output.Length > 0)
+					Debug.Log ("** output: \n" + output);
+				string error = stderr.ToString ();
+				if (error.Length > 0)
+					Debug.Log ("** error: \n" + error);
+
+				if (process.ExitCode != 0) {
+					throw new Exception ("Failed running " + ExeName + " error: " + process.ExitCode + " : " + error);
+				}
+			}
+		}}
+}
+

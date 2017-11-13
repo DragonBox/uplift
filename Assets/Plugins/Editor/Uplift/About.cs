563a1aef41f037e8dd9e90896b46fa3cf78bc796
Paul Niezborala <paulniezbo@gmail.com>
2017-11-13 18:26:00 +0100

    Add release tools
    Add rake based release tools to easily deploy new version and its
    artifacts.

--- Assets/Plugins/Editor/Uplift/About.cs
+++ Assets/Plugins/Editor/Uplift/About.cs
@@ -26,7 +26,7 @@ namespace Uplift
 {
     internal static class About
     {
-        public static readonly string Version = "1.0.0beta1";
+        public static readonly string Version = "1.0.0beta2";
         public static readonly string[] Authors = new string[]
         {
             "Przemysław Kamiński",
--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ ruby "2.3.3"
 source 'https://rubygems.org'
 
 gem 'u3d', '>= 1.0.3'
+gem 'github_api', '>= 0.18.0'
 

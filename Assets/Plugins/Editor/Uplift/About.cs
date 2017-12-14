b54d04144d173bb366c4f4027a0809c65a872bf7
Paul Niezborala <paulniezbo@gmail.com>
2017-12-14 14:28:14 +0100

    Bump version to 1.0.0beta8

--- Assets/Plugins/Editor/Uplift/About.cs
+++ Assets/Plugins/Editor/Uplift/About.cs
@@ -26,7 +26,7 @@ namespace Uplift
 {
     internal static class About
     {
-        public static readonly string Version = "1.0.0beta7";
+        public static readonly string Version = "1.0.0beta8";
         public static readonly string[] Authors = new string[]
         {
             "Przemysław Kamiński",
